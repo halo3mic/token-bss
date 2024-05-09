223be31b061c5e957fc400f08a2f607ccdf053c0
halo3mic <46010359+halo3mic@users.noreply.github.com>
2024-05-09 15:17:32 +0100

    fix script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,17 +5,17 @@ set -e
 echo "Installing token-bss..."
 
 BASE_DIR=${HOME}
-ERC20_TOPUP_BIN_DIR="${BASE_DIR}/.token-bss/bin"
-EXECUTABLE_PATH="${ERC20_TOPUP_BIN_DIR}/token-bss"
+TOKEN_BSS_BIN_DIR="${BASE_DIR}/.token-bss/bin"
+EXECUTABLE_PATH="${TOKEN_BSS_BIN_DIR}/token-bss"
 
-cargo build --release
+cargo build --bin token-bss-cli --release
 if [ $? -ne 0 ]; then
     echo "Cargo build failed, exiting."
     exit 1
 fi
 
-mkdir -p "${ERC20_TOPUP_BIN_DIR}"
-cp "./target/release/token-bss" "${EXECUTABLE_PATH}"
+mkdir -p "${TOKEN_BSS_BIN_DIR}"
+cp "./target/release/token-bss-cli" "${EXECUTABLE_PATH}"
 chmod +x "${EXECUTABLE_PATH}"
 
 case $SHELL in
@@ -26,12 +26,12 @@ case $SHELL in
         PROFILE="${HOME}/.bashrc"
         ;;
     *)
-        echo "Could not detect shell, manually add ${ERC20_TOPUP_BIN_DIR} to your PATH."
+        echo "Could not detect shell, manually add ${TOKEN_BSS_BIN_DIR} to your PATH."
         exit 1
 esac
 
-if [[ ":$PATH:" != *":${ERC20_TOPUP_BIN_DIR}:"* ]]; then
-    echo >> "${PROFILE}" && echo "export PATH=\"\$PATH:${ERC20_TOPUP_BIN_DIR}\"" >> "${PROFILE}"
+if [[ ":$PATH:" != *":${TOKEN_BSS_BIN_DIR}:"* ]]; then
+    echo >> "${PROFILE}" && echo "export PATH=\"\$PATH:${TOKEN_BSS_BIN_DIR}\"" >> "${PROFILE}"
 fi
 
 echo && echo "Added token-bss to PATH in ${PROFILE}. If the script was not sourced, run 'source ${PROFILE}' or start a new terminal session to use token-bss."
