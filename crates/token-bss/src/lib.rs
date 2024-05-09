20a5059cce06f7b54774e437af96ff711966b483
halo3mic <46010359+halo3mic@users.noreply.github.com>
2024-05-09 13:59:38 +0100

    refactor into workspace

--- crates/token-bss/src/lib.rs
+++ crates/token-bss/src/lib.rs
@@ -1,9 +1,15 @@
-#![allow(clippy::module_inception)]
+mod trace_parser;
+mod slot_finder;
+mod lang;
+mod utils;
+mod ops;
+
+
+pub use slot_finder::find_balance_slots_and_update_ratio;
+pub use lang::EvmLanguage;
 
 mod common;
-mod slot_finder;
 
-pub use slot_finder::EvmLanguage;
 pub use common::TraceFn;
 use common::*;
 
